0f1d818e3b8cb43ad8a57010c8f6a27926970698
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2023-11-24 11:22:38 -0500

    Update phish.js

--- phish.js
+++ phish.js
@@ -1,5 +1,5 @@
 // I am not 100% sure all of these are technically valid domains
-var domainlist = ["another.com","example.com","example.net","èxample.com","example.xyz","secure.example.com","domain.com","google.com","secureexample.com","examplecom.evil.com","example.co","example.com","example.com","example.evil.com","eample.com","example.com.evil.com","exɑmple.com","exampple.com","secure.example","secure.examplė.com","phisher.example","com.example","example.online","securityexample.com","Ēxample.com","exampIe.com","exanple.com","exampl.com","examplc.com","examplec.om","exam.ple.com","examp1e.com","examp|e.com","exampļe.com","examplelogin.domain.com","secure.domain.com","loginexample.com","ex.amp.le.com","example.cam","3xample.com", "examp1.com", "httpsexample.com","http.sexample.com","example.comlogin.online", "secure.example.com.github.io", "exarnple.com", "exaɱple.com", "eexample.com", "examples.com", "ex@mple.com", "examp1e.com", "exampÍe.com", "eamplecom.com", "eample.com", "exampɫe.com", "exɒmple.com", "ɛxample.com", "examlple.com", "exannple.com", "exaɴɴple.com", "e.xam.ple.com", "securee@ample.com", "secure.exaʬple.com"]
+var domainlist = ["another.com","example.com","example.net","èxample.com","example.xyz","secure.example.com","domain.com","google.com","secureexample.com","examplecom.evil.com","example.co","example.com","example.com","example.evil.com","eample.com","example.com.evil.com","exɑmple.com","exampple.com","secure.example","secure.examplė.com","phisher.example","com.example","example.online","securityexample.com","Ēxample.com","exampIe.com","exanple.com","exampl.com","examplc.com","examplec.om","exam.ple.com","examp1e.com","examp|e.com","exampļe.com","examplelogin.domain.com","secure.domain.com","loginexample.com","ex.amp.le.com","example.cam","3xample.com", "examp1.com", "httpsexample.com","http.sexample.com","example.comlogin.online", "secure.example.com.github.io", "exarnple.com", "exaɱple.com", "eexample.com", "examples.com", "ex@mple.com", "examp1e.com", "exampÍe.com", "eamplecom.com", "eample.com", "exampɫe.com", "exɒmple.com", "ɛxample.com", "examlple.com", "exannple.com", "exaɴɴple.com", "e.xam.ple.com", "securee@ample.com", "secure.exaʬple.com", "xn--exmple-jxc.com", "secure@example.com]
 var alloweddomains = ['example.com',"secure.example.com"]
 var list = document.getElementById('spoofed')
 for(var t = 0;t < domainlist.length;t++){
@@ -11,7 +11,11 @@ for(var t = 0;t < domainlist.length;t++){
   checkbox.type = "checkbox"
   checkbox.onclick = function(){
     var line = this.parentElement
-    if(alloweddomains.includes(domain.textContent) === true){
+    let d = domain.textContent;
+    if(d.includes("@")){
+      d = d.split("@")[1]
+    }
+    if(alloweddomains.includes(d) === true){
       line.style.color = 'green'
     }
     else{
